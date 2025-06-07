2183531ace0ba55b95908240223514a4db34f804
SuJerry01 <sujerry0525@gmail.com>
2025-06-08 00:13:34 +0800

    Initial commit

---
+++ STM32平台教程/STM32F767ZITx控制电机例程/MDK-ARM/GO-M8010-6-demo/startup_stm32f767xx.d
@@ -0,0 +1,1 @@
+go-m8010-6-demo\startup_stm32f767xx.o: startup_stm32f767xx.s
---
+++ STM32平台教程/STM32F767ZITx控制电机例程/MDK-ARM/RTE/_GO-M8010-6-demo/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GO-M8010-6-demo' 
+ * Target:  'GO-M8010-6-demo' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
